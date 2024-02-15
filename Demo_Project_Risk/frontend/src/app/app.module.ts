bbfa95bbe085cb244a8d496c4cf0fd5aee1de139
Maloo <Mayank.Maloo@bakerhughes.com>
2024-02-15 16:26:24 +0530

    Files are added

--- Demo_Project_Risk/frontend/src/app/app.module.ts
+++ Demo_Project_Risk/frontend/src/app/app.module.ts
@@ -6,20 +6,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ItemNameComponent } from './components/item-name/item-name.component';
 import {MatTableModule} from '@angular/material/table';
 import { TableDisplayComponent } from './components/table-display/table-display.component'
-import {
-  CdkDragDrop,
-  CdkDrag,
-  CdkDropList,
-  CdkDropListGroup,
-  moveItemInArray,
-  transferArrayItem,
-} from '@angular/cdk/drag-drop';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {A11yModule} from '@angular/cdk/a11y';
 
 
 import {Sort, MatSortModule} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table'
+// import {MatTableDataSource} from '@angular/material/table'
+
 import {MatIconModule} from '@angular/material/icon'
 
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+// import {MaterialModule} from './material/material.module'
  
 @NgModule({
   declarations: [
@@ -28,15 +32,27 @@ import {MatIconModule} from '@angular/material/icon'
     TableDisplayComponent,
   ],
   imports: [
+
+   
     
+   
 
     BrowserModule,
     BrowserAnimationsModule,
     MatTableModule,
-    CdkDrag,
-    CdkDropListGroup,
+      
+    A11yModule,
+    CdkTreeModule,
+    CdkTableModule,
+    ScrollingModule,
+  
+    DragDropModule,
     MatSortModule,
-    MatIconModule
+    MatIconModule,
+
+    HttpClientModule,
+
+
     
     
     
---
+++ Demo_Project_Risk/frontend/src/app/material/material.module.ts
@@ -0,0 +1,32 @@
+// import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatTable } from '@angular/material/table';
+// import { MatPaginator } from '@angular/material/paginator';
+// import { MatSortModule } from '@angular/material/sort';
+
+// MatTableDataSourcePaginator,
+
+
+const MaterialUI = [
+  // NgModule,
+  // CommonModule,
+
+  MatTable,
+  
+
+
+
+];
+
+
+@NgModule({
+  
+  imports: [
+    ...MaterialUI
+  ],
+
+  
+  exports:[...MaterialUI]
+})
+export class MaterialModule { }
